643014f9937a7b38bbddc7afd8bff8ec392e2678
elcode7 <101850419+elcode7@users.noreply.github.com>
2026-01-13 21:00:16 -0800

    Add addNumbers function

--- touch journal.js
+++ touch journal.js
@@ -2,3 +2,9 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Adds two numbers and returns the result.
+function addNumbers (a, b) {
+  return a + b ;
+}
+console.log(add.Numbers (3, 5)); // 8
