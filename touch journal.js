53e95fc3b0f76ec288ef17757a221ffe5bd1cc50
elcode7 <101850419+elcode7@users.noreply.github.com>
2026-01-13 21:04:55 -0800

    Add journal.js with number functions

--- touch journal.js
+++ touch journal.js
@@ -8,3 +8,10 @@ function addNumbers (a, b) {
   return a + b ;
 }
 console.log(add.Numbers (3, 5)); // 8
+
+// Checks if a number is even
+function isEven(number) {
+  return number % 2 === 0;
+}
+console.log (isEven(4)); // true
+console.log (isEven(7)); // false
